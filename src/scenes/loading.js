53f6821c838ed902778c63dd7f13ca54b63480b3
Daniel Fackrell <unlearned@gmail.com>
2018-09-29 10:11:02 -0600

    Removed unused rooms

--- src/scenes/loading.js
+++ src/scenes/loading.js
@@ -13,36 +13,7 @@ require('../../assets/images/sprite_atlas.png');
 require('../../assets/images/map_tileset.png');
 require('../../assets/images/play.png');
 
-require('../../assets/json/room1.json');
-require('../../assets/json/room2.json');
-require('../../assets/json/room3.json');
-require('../../assets/json/room4.json');
-require('../../assets/json/room5.json');
-require('../../assets/json/room6.json');
-require('../../assets/json/room7.json');
-require('../../assets/json/room8.json');
-require('../../assets/json/room9.json');
-require('../../assets/json/room10.json');
-require('../../assets/json/room11.json');
-require('../../assets/json/room12.json');
-require('../../assets/json/room13.json');
-require('../../assets/json/room14.json');
 require('../../assets/json/room15.json');
-require('../../assets/json/room16.json');
-require('../../assets/json/room17.json');
-require('../../assets/json/room18.json');
-require('../../assets/json/room19.json');
-require('../../assets/json/room20.json');
-require('../../assets/json/room21.json');
-require('../../assets/json/room22.json');
-require('../../assets/json/room23.json');
-require('../../assets/json/room24.json');
-require('../../assets/json/room25.json');
-require('../../assets/json/room26.json');
-require('../../assets/json/room27.json');
-require('../../assets/json/room28.json');
-require('../../assets/json/room29.json');
-require('../../assets/json/room30.json');
 
 export default class LoadingScene extends Phaser.Scene {
     constructor (config, key = 'Loading') {
