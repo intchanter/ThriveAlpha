7d94149a3b119ace17febfc4c87490226f1ca9b8
Rob Brown <rob@betterservers.com>
2018-09-29 11:40:33 -0600

    Comment

--- windows_gitbash_install_make.sh
+++ windows_gitbash_install_make.sh
@@ -1,8 +1,9 @@
 #/usr/bin/env sh
 
-# Ming folder
+# Use latest version
 export WINDOWS_MAKE_URL=https://versaweb.dl.sourceforge.net/project/ezwinports/make-4.2.1-without-guile-w32-bin.zip
 export WINDOWS_MAKE_BASE=`basename $WINDOWS_MAKE_URL`
+# Find Ming folder
 export DST=`echo /c/Program*/Git/ming*`
 [ !-z $WINDOWS_MAKE_BASE ] || curl -o $WINDOWS_MAKE_BASE $WINDOWS_MAKE_URL
 unzip -d "$DST" "$WINDOWS_MAKE_BASE"
