6e9bca7d5c1cea2e178b5514ac75eca4a63e1fd3
Rob Brown <rob@betterservers.com>
2018-09-29 13:05:16 -0600

    Rollback windows Make from 4.x to 3.x

--- windows_gitbash_install_make.sh
+++ windows_gitbash_install_make.sh
@@ -1,7 +1,7 @@
 #/usr/bin/env sh
 
 # Use latest version
-export WINDOWS_MAKE_URL=https://versaweb.dl.sourceforge.net/project/ezwinports/make-4.2.1-without-guile-w32-bin.zip
+export WINDOWS_MAKE_URL=https://versaweb.dl.sourceforge.net/project/gnuwin32/make/3.81/make-3.81-bin.zip
 export WINDOWS_MAKE_BASE=`basename $WINDOWS_MAKE_URL`
 # Find Ming folder
 export DST=`echo /c/Program*/Git/ming*`
