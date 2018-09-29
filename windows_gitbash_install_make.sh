635d3df2cf58b9c6d1ace607aef62084d0b68cac
Rob Brown <rob@betterservers.com>
2018-09-29 11:23:06 -0600

    Initial add of GitBash Make installer script

---
+++ windows_gitbash_install_make.sh
@@ -0,0 +1,8 @@
+#/usr/bin/env sh
+
+# Ming folder
+export WINDOWS_MAKE_URL=https://versaweb.dl.sourceforge.net/project/ezwinports/make-4.2.1-without-guile-w32-bin.zip
+export WINDOWS_MAKE_BASE=`basename $WINDOWS_MAKE_URL`
+export DST=`echo /c/Program*/Git/ming*`
+[ !-z $WINDOWS_MAKE_BASE ] || curl -o $WINDOWS_MAKE_BASE $WINDOWS_MAKE_URL
+unzip -d $DST $WINDOWS_MAKE_BASE
